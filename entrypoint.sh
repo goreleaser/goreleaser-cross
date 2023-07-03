74d642cc4685474a3cb110a059a70ae5de12fbb4
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-07-03 17:13:40 +0000

    fix: entrypoint.sh login
    closes #40

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,31 @@ if [[ -f $DOCKER_CREDS_FILE ]]; then
 	fi
 fi
 
+if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
+	echo "Login to the docker..."
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
+fi
+
+# Workaround for github actions when access to different repositories is needed.
+# Github actions provides a GITHUB_TOKEN secret that can only access the current
+# repository and you cannot configure it's value.
+# Access to different repositories is needed by brew for example.
+
+if [ -n "$GORELEASER_GITHUB_TOKEN" ] ; then
+	export GITHUB_TOKEN=$GORELEASER_GITHUB_TOKEN
+fi
+
+if [ -n "$GITHUB_TOKEN" ]; then
+	# Log into GitHub package registry
+	echo "$GITHUB_TOKEN" | docker login docker.pkg.github.com -u docker --password-stdin
+	echo "$GITHUB_TOKEN" | docker login ghcr.io -u docker --password-stdin
+fi
+
+if [ -n "$CI_REGISTRY_PASSWORD" ]; then
+	# Log into GitLab registry
+	echo "$CI_REGISTRY_PASSWORD" | docker login "$CI_REGISTRY" -u "$CI_REGISTRY_USER" --password-stdin
+fi
+
 git config --global --add safe.directory "$(pwd)"
 
 exec goreleaser "$@"
