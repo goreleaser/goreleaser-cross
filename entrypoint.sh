df843fc30e9ac0fa1ebee4b269be267690344120
Artur Troian <troian.ap@gmail.com>
2023-08-01 10:59:30 -0400

    feat: export CC/CXX depending on the host arch (#47)
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 if [[ -z "$GPG_KEY" ]]; then
 	GPG_KEY=/secrets/key.gpg
@@ -18,10 +18,12 @@ if [[ -z "$DOCKER_CREDS_FILE" ]]; then
 fi
 
 if [[ -f $DOCKER_CREDS_FILE ]]; then
-	if cat "$DOCKER_CREDS_FILE" | jq >/dev/null 2>&1 ; then
+	if jq < "$DOCKER_CREDS_FILE" >/dev/null 2>&1 ; then
+	    # shellcheck disable=SC2046
+		# shellcheck disable=SC2162
 		while read user pass registry ; do
 			echo "$pass" | docker login --username "$user" --password-stdin "$registry"
-		done <<< $(cat "$DOCKER_CREDS_FILE" | jq -Mr '.registries[] | [.user, .pass, .registry] | @tsv')
+		done <<< $(jq -Mr '.registries[] | [.user, .pass, .registry] | @tsv' < "$DOCKER_CREDS_FILE")
 	else
 		IFS=':'
 			while read -r user pass registry; do
@@ -54,6 +56,24 @@ if [ -n "$CI_REGISTRY_PASSWORD" ]; then
 	# Log into GitLab registry
 	echo "$CI_REGISTRY_PASSWORD" | docker login "$CI_REGISTRY" -u "$CI_REGISTRY_USER" --password-stdin
 fi
+
+case "$(uname -m)" in
+    x86_64|amd64)
+        CC=x86_64-linux-gnu-gcc
+        CXX=x86_64-linux-gnu-g++
+        ;;
+    arm64|aarch64)
+        CC=aarch64-linux-gnu-gcc
+        CXX=aarch64-linux-gnu-g++
+    ;;
+    *)
+        echo "goreleaser-cross supports only amd64/arm64 hosts"
+        exit 1
+        ;;
+esac
+
+export CC
+export CXX
 
 git config --global --add safe.directory "$(pwd)"
 
